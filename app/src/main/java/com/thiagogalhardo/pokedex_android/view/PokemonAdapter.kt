4dbf4a0b1eea0ab4aa7a6c183326218a92f2d137
Thiago Galhardo <thiago.galhardo@hotmail.com>
2021-12-01 16:22:49 -0300

    feat: add colors based on types

--- app/src/main/java/com/thiagogalhardo/pokedex_android/view/PokemonAdapter.kt
+++ app/src/main/java/com/thiagogalhardo/pokedex_android/view/PokemonAdapter.kt
@@ -1,20 +1,22 @@
 package com.thiagogalhardo.pokedex_android.view
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.thiagogalhardo.pokedex_android.R
 import com.thiagogalhardo.pokedex_android.domain.Pokemon
+import kotlinx.android.synthetic.main.pokemon_item.view.*
 
 class PokemonAdapter(
     private val items: List<Pokemon?>
 
 ) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.pokemon_item, parent, false)
@@ -25,15 +27,35 @@ class PokemonAdapter(
 
         val item = items[position]
         holder.bindView(item)
-
-
     }
 
     override fun getItemCount() = items.size
 
-
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        init {
+            itemView.setOnClickListener() { v: View ->
+                val position: Int = adapterPosition
+                Toast.makeText(
+                    itemView.context,
+                    "Click in item ${position + 1}",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                if (position == 0) {
+
+                    Toast.makeText(
+                        itemView.context,
+                        "BINGO ${position + 1}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    val context = itemView.context
+                    var mIntent = Intent(context, PokemonDetails::class.java)
+                    context.startActivity(mIntent)
+                }
+            }
+        }
 
         fun bindView(item: Pokemon?) = with(itemView) {
             val ivPokemon = findViewById<ImageView>(R.id.ivPokemon)
@@ -42,7 +64,6 @@ class PokemonAdapter(
             val tvType1 = findViewById<TextView>(R.id.tvType1)
             val tvType2 = findViewById<TextView>(R.id.tvType2)
 
-
             item?.let {
 
                 Glide.with(itemView.context).load(it.imageUrl).into(ivPokemon)
@@ -50,6 +71,90 @@ class PokemonAdapter(
                 tvNumber.text = "Nº ${item.formattedNumber}"
                 tvName.text = item.name.capitalize()
                 tvType1.text = item.types[0].name
+                var backgroudCard = cv_pokemon
+
+
+
+
+                if (tvType1.text.equals("normal")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.normalType))
+                }
+                if (tvType1.text.equals("poison")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.poisonType))
+                }
+
+                if (tvType1.text.equals("psychic")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.psychicType))
+                }
+
+                if (tvType1.text.equals("grass")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.grassType))
+                }
+
+                if (tvType1.text.equals("ground")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.groundType))
+                }
+
+                if (tvType1.text.equals("ice")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.iceType))
+                }
+
+                if (tvType1.text.equals("fire")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.fireType))
+                }
+
+                if (tvType1.text.equals("rock")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.rockType))
+                }
+
+                if (tvType1.text.equals("dragon")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.dragonType))
+                }
+
+                if (tvType1.text.equals("water")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.waterType))
+                }
+
+
+                if (tvType1.text.equals("bug")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.bugType))
+                }
+
+
+                if (tvType1.text.equals("dark")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.darkType))
+                }
+
+                if (tvType1.text.equals("fighting")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.fightingType))
+                }
+
+                if (tvType1.text.equals("ghost")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.ghostType))
+                }
+
+                if (tvType1.text.equals("steel")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.steelType))
+                }
+
+                if (tvType1.text.equals("flying")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.flyingType))
+                }
+
+                if (tvType1.text.equals("electric")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.electricType))
+                }
+
+                if (tvType1.text.equals("fairy")) {
+                    backgroudCard.setCardBackgroundColor(resources.getColor(R.color.fairyType))
+                }
+
+
+
+
+
+
+
 
                 if (item.types.size > 1) {
                     tvType2.visibility = View.VISIBLE
